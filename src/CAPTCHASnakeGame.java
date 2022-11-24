7b36a317979a66f664655d438f9bd92d4896235b
kai rowley <zerenitylife@gmail.com>
2022-11-24 15:55:16 +0100

    ok

--- src/CAPTCHASnakeGame.java
+++ src/CAPTCHASnakeGame.java
@@ -1,5 +1,10 @@
 public class CAPTCHASnakeGame {
     public static void main(String[] args) {
-        System.out.println("hello kai");                
+        System.out.println("hello kai"); 
+        System.out.println("ijfewjfewfjlwekfjelfk"); 
+        //heokwof
+        //heufhewifh     
+        System.out.println("hello");       
+    }               
     }
 }
